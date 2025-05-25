d565238daa9bf496d482d9edc806e3c401e38385
Alex <86182397+alehxs@users.noreply.github.com>
2025-05-25 17:09:05 -0500

    fix: update SECRET_KEY handling and correct DB env variable typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_b^wl@cs&s1*9s%+w$u75-8*u*)exy(o(v8!rwyj3756q^wxi='
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +79,7 @@ DATABASES = {
       'ENGINE': 'django.db.backends.postgresql',
       'HOST': os.environ.get('DB_HOST'),
       'NAME': os.environ.get('DB_NAME'),
-      'NAME': os.environ.get('DB_USER'),
+      'USER': os.environ.get('DB_USER'),
       'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
