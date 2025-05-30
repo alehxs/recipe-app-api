7eaa0e399902dd35e9a59159c6e1827e31962fce
Alex <86182397+alehxs@users.noreply.github.com>
2025-05-29 23:20:05 -0500

    feat: validate user creation requires email and add test for missing email case

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -25,3 +25,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+            
